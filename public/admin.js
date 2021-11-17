68ec687a6fd2214fbe11f187fc491fd4a50163f7
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-11-17 08:55:20 -0500

    JS-BUILDING-AN-ADMIN-PORTAL

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,21 @@
 
-// Your Code Here
+async function main() {
+    let response = await fetch ('http://localhost:3000/api-docs.html#operation/listBooks', {
+        method:'GET',
+});
+
+let books = await response.json();
+console.log(books);
+}
+    let response = await fetch ('http://localhost:3000/api-docs.html#operation/updateBook', {
+        method:'PATCH',
+        headers {
+            'Content-Type': 'application/json'
+},
+
+body:   
+
+})
+
+}
+
