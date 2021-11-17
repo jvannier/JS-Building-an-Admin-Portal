715dc9570dffa15fafb356fc200ccc23d2ba4734
Thomas Selden <thomasselden42@gmail.com>
2021-11-17 17:01:17 -0500

    got the books to appear listed but couldn't figure out how to edit and save the book quantity

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,30 @@
-
 // Your Code Here
+
+async function main() {
+  let response = await fetch("http://localhost:3001/listBooks", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let bookListObj = await response.json();
+  bookListObj.forEach(renderBook);
+}
+
+function renderBook(book) {
+  let bookContainer = document.querySelector("#root");
+  bookContainer.innerHTML += `
+        <div class="col-sm-3">
+                <div class="card-body">
+                    <h3 class="card-title">${book.title}</h3>
+                    <h4 class="quantity">Available: ${book.quantity}</h4>
+                </div>
+            
+        </div>
+    `;
+  let bookQuantity = book.quantity;
+  let myBtn = document.createElement("btn");
+  
+}
+
+main();
--- public/index.js
+++ public/index.js
@@ -1,7 +1,10 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
+    let response = await fetch('http://localhost:3001/listBooks', {
+        method: "GET"})
+
     let books = await response.json()
+    console.log(books)
 
     books.forEach(renderBook)
 }
