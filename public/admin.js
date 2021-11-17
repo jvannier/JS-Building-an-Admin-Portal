5157aee3824239e4d1095e2dabd1c77416e6cb4b
Omar <Omar>
2021-11-16 23:16:11 -0500

    added asyncs functions, createdElements and a submit button with onclick

--- public/admin.js
+++ public/admin.js
@@ -11,3 +11,33 @@ async function updateBook() {
   console.log(updatedBook);
 }
 // Your Code Here
+
+async function displayBooks() {
+  let response = await fetch("http://localhost:3001/listBooks");
+  let books = await response.json();
+  for (let i = 0; i < books.length; i++) {
+    let list = document.createElement("li");
+    list.innerHTML = books[i].title;
+    document.body.append(list);
+    let input = document.createElement("input");
+    list.append(input);
+    let submit = document.createElement("button");
+    submit.addEventListener("click", newValue(books[i].id, input.value));
+    submit.textContent = "submit";
+    list.append(submit);
+  }
+}
+displayBooks();
+
+async function newValue(bookId, value) {
+  let response = await fetch("http://localhost:3001/updateBook", {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id: `${bookId}`,
+      quantity: `${value}`,
+    }),
+  });
+  let newBook = await response.json();
+  console.log(newBook);
+}
