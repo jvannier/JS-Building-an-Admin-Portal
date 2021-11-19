6457978f1db5b7a8377451ff07c875ec3c32c9d3
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-11-19 06:57:01 -0600

    remove solution

--- public/admin.js
+++ public/admin.js
@@ -2,19 +2,3 @@
 // Your Code Here
 
 const base_api_url = ""
-async function updateBook(bookId) {
-    let updateRequestBody = { 
-        "id": 1, 
-        "title": "Jonathon AI", 
-        "description": "The reality-bending adventures of a clandestine service agency in the year 2166", 
-        "year": 2014, 
-        "quantity": "10", 
-        "imageURL": "/assets/shinobi-initiative.jpeg" }
-    let response = await fetch(`${base_api_url}/updateBook`, {
-        method: "PATCH",
-        body: JSON.stringify(
-            updateRequestBody
-        )
-    })
-    console.log(await response.json())
-}
